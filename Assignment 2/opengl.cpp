0603a77e1e068bcef48fe2506e1271c02b05c0db
Adwait Nagarkar <109037318+Adwait47s@users.noreply.github.com>
2023-04-20 07:04:34 +0530

    Update opengl.cpp

--- Assignment 2/opengl.cpp
+++ Assignment 2/opengl.cpp
@@ -38,52 +38,101 @@ void simpleline(float x, float y, float x1,float y1){
 
 }
 
-void bres(int x,int y, int x1,int y1){
-	
-	int dy = y1-y;
-	int dx = x1-x;
-	int p_in = 2*dy - dx;
-	
-	displayfunc(x,y);
-	while(x1>x){
-		x++;
-		if(p_in>=0){
-		y++;
-			p_in += 2*dy - 2*dx;
-		}
-		else{
-			p_in += 2*dy;
-			
-		}
-		displayfunc(x,y);
-	}
+void BSA_Algo(int xi, int yi, int xf, int yf)
+{
+/*
+* Input : Initial (xi, yi) and final co-ordinates of line segment.
+* Utility : Plot line on the window using Bresenham's line drawing algorithm.
+* Output : Line.
+*/
+int P;
+int dx = xf - xi;
+int dy = yf - yi;
+int x = xi, y = yi;
+//Consider absolute value of dx and dy.
+if (dx < 0)
+{
+dx = -dx;
 }
-void bresenham(int xA,int yA, int xB,int yB)
+if (dy < 0)
 {
-
-
-GLint dy = xB - xA;
-GLint dx = yB - yA;
-GLint p_ini = 2 * dy - dx;
-GLint x_plot = xA;
-GLint y_plot = yA;
-displayfunc(x_plot, y_plot);
-while (x_plot < xB)
+dy = -dy;
+}
+//Change of x and y values depends on initial and final points defined by the user.
+int x_change = 1, y_change = 1;
+if(xi > xf)
 {
-x_plot += 1;
-if (p_ini <= 0)
+x_change = -1;
+}
+if (yi > yf)
 {
-p_ini = p_ini + 2 * dy;
+y_change = -1;
+}
+//plot initial point.
+glVertex2i(x, y);
+//For horizontal line, increment y value. Plot the line and return.
+if (dx == 0)
+{
+glBegin(GL_POINTS);
+for (int i = 1; i<dy; i++)
+{
+y = y + y_change;
+glVertex2i(x, y);
+}
+glEnd();
+return;
+}
+//For vertical line, increment x value. Plot the line and return.
+if (dy == 0)
+{
+glBegin(GL_POINTS);
+for (int i = 1; i<dx; i++)
+{
+x = x + x_change;
+glVertex2i(x, y);
+}
+glEnd();
+return;
+}
+//Calculate vertex position based on descision parameter.
+glBegin(GL_POINTS);
+if (dx > dy)
+{
+P = 2*dy -dx;
+for(int i = 0; i < dx; i++)
+{
+if (P > 0)
+{
+y += y_change;
+P = P + 2*(dy - dx);
 }
 else
 {
-y_plot += 1;
-p_ini = p_ini + 2 * dy - 2 * dx;
+P = P + 2*dy;
 }
-
-glPointSize(1);
-displayfunc(x_plot, y_plot);
+x += x_change;
+glVertex2i(x, y);
 }
+}
+else
+{
+P = 2*dx -dy;
+for(int i = 0; i < dy; i++)
+{
+if (P > 0)
+{
+x += x_change;
+P = P + 2*(dx - dy);
+}
+else
+{
+P = P + 2*dx;
+}
+y += y_change;
+glVertex2i(x, y);
+}
+}
+glEnd();
 }
 void renderFunction()
 {
@@ -98,14 +147,14 @@ void renderFunction()
         glVertex2f(0.5, 0.5);
         glVertex2f(0.5, -0.5);
     glEnd();*/
-    bres(50, 100, 650,100);
-    bres(50, 100, 50,300);
-    bres(50, 300, 250,300);
-    bres(250, 300, 250,500);
-    bres(250, 500, 450,500);
-    bres(450, 500, 450,699);
-    bres(450, 699, 650,699);
-    bres(650, 699, 650,100);
+    BSA_Algo(50, 100, 650,100);
+    BSA_Algo(50, 100, 50,300);
+    BSA_Algo(50, 300, 250,300);
+    BSA_Algo(250, 300, 250,500);
+    BSA_Algo(250, 500, 450,500);
+    BSA_Algo(450, 500, 450,699);
+    BSA_Algo(450, 699, 650,699);
+    BSA_Algo(650, 699, 650,100);
     glFlush();
     
 }
@@ -121,4 +170,3 @@ int main(int argc, char** argv)
     glutMainLoop();    
     return 0;
 }
-
